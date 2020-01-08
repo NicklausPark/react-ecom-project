7c72f0f158a01647cbc12dd417dda15120bec6d7
nickdoescode <nicklaus.park@gmail.com>
2020-01-07 23:26:48 -1000

    added cartIcon and cartDropdown components to our application, also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,39 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-import "./header.styles.scss";
-import { auth } from "../../firebase/firebase.utils";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './header.styles.scss';
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
-  <div className='header'>
-    <Link to='/'>
-      <Logo className='logo-container' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/contact'>
-        CONTACT
-      </Link>
-      <Link className='option' to='/signin'>
-        {currentUser ? (
-          <div className='option' onClick={() => auth.signOut()}>
-            SIGN OUT
-          </div>
-        ) : (
-          <Link className='option' to='/signin'>
-            SIGN IN
-          </Link>
-        )}
-      </Link>
-    </div>
-  </div>
+const Header = ({ currentUser, hidden }) => (
+	<div className='header'>
+		<Link to='/'>
+			<Logo className='logo-container' />
+		</Link>
+		<div className='options'>
+			<Link className='option' to='/shop'>
+				SHOP
+			</Link>
+			<Link className='option' to='/contact'>
+				CONTACT
+			</Link>
+			<Link className='option' to='/signin'>
+				{currentUser ? (
+					<div className='option' onClick={() => auth.signOut()}>
+						SIGN OUT
+					</div>
+				) : (
+					<Link className='option' to='/signin'>
+						SIGN IN
+					</Link>
+				)}
+			</Link>
+			<CartIcon />
+		</div>
+		{hidden ? null : <CartDropdown />}
+	</div>
 );
 
-const mapStatetoProps = state => ({
-  currentUser: state.user.currentUser
+const mapStatetoProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden
 });
 
 export default connect(mapStatetoProps)(Header);
